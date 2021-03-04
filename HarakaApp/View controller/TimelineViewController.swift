77a5793892d6781826c33b9686bb40fb97afd1e7
Njood <njood1999@gmail.com>
2021-03-05 00:38:29 +0300

    hh

--- HarakaApp/View controller/TimelineViewController.swift
+++ HarakaApp/View controller/TimelineViewController.swift
@@ -11,52 +11,54 @@ import Firebase
 class TimelineViewController: UITableViewController {
     
     var posts:[Post]?
- //   var ref:  DatabaseReference!
- //   var ref = Database.database().reference()
+    var ref:  DatabaseReference!
     var postrefs: [DataSnapshot]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     //   fetchPosts()
         
         tableView.separatorStyle = .none
         tableView.estimatedRowHeight = tableView.rowHeight
         tableView.rowHeight = UITableView.automaticDimension
         
-   //     ref = Database.database().reference()
-        var ref:  DatabaseReference!
         ref = Database.database(url: "https://haraka-73619-default-rtdb.firebaseio.com/").reference()
-        ref.child("posts").getData {(error, snapshot) in if let error = error {
-            
-        } else if snapshot.exists() {
-            
-            for child in snapshot.children{
-                let postDict = snapshot.value as? [String : AnyObject] ?? [:]
-                let usern = postDict["username"] as? String ?? ""
-                let times = postDict["timestamp"] as? String ?? ""
-                let cap = postDict["caption"] as? String ?? ""
-                let nol = postDict["numOfLikes"] as? Int ?? 0
-                let noc = postDict["numOfComments"] as? Int ?? 0
-                let id = postDict["id"] as? String ?? ""
-                
-                let postUser = User(usernameUI: usern, profileImage: UIImage(named:"figure.walk.circle"))
-                let newPost = Post(createdBy: postUser, timeAgo: times, captionUI: cap, numOfLikesUI: nol, numOfCommentsUI: noc, postID: id)
-                self.posts?.append(newPost)
-            }
-            self.tableView.reloadData()
-            
-        }else{
-            
-        }}
-    }
+        
+        fetchPosts()
+        tableView.reloadData()
+        }
     
-    override func viewDidAppear(_ animated: Bool) {
-
-    }
-    
-    func setObservers(){
-
-    }
+    func fetchPosts(){
+        
+        // retrieve posts from database, may return error or snapshot (snapshot contains data)
+        self.ref.child("posts").getData(completion: { (error, snapshot) in
+            
+            if let error = error{ print(error.localizedDescription) }
+            
+            else{
+                if(snapshot.exists()){
+                        
+                    for child in snapshot.children.allObjects as! [DataSnapshot]{
+                        guard let postDict = child.value as?[String:Any] else{ return }
+                        let usern = postDict["username"] as? String ?? ""
+                        let times = postDict["timestamp"] as? String ?? ""
+                        let cap = postDict["caption"] as? String ?? ""
+                        let nol = postDict["numOfLikes"] as? Int ?? 0
+                        let noc = postDict["numOfComments"] as? Int ?? 0
+                        let id = postDict["id"] as? String ?? ""
+                        
+                        let postUser = User(usernameUI: usern, profileImage: UIImage(named:"figure.walk.circle"))
+                        let newPost = Post(createdBy: postUser, timeAgo: times, captionUI: cap, numOfLikesUI: nol, numOfCommentsUI: noc, postID: id)
+                        self.posts?.append(newPost)
+                        
+                    } // End of loop
+                } // End of If (snapshot.exists)
+                    
+            } // End of else
+            
+        }) // End of retrieving function
+        
+        
+    } // End of fetchPosts method
     
 }
 
@@ -79,14 +81,6 @@ extension TimelineViewController{
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("your row number: \(indexPath.row)")
-        
-     //
-        _ = tableView.cellForRow(at: indexPath)
-        
     }
     
 }
