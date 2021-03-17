55472c80258f5a98c1ba026294c430cc4c0cb1df
Njood <njood1999@gmail.com>
2021-03-17 14:14:28 +0300

    d

---
+++ HarakaApp/View controller/GameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GameViewController.swift
+//  HarakaApp
+//
+//  Created by Njood Alhajery on 17/03/2021.
+//
+
+import UIKit
+
+class GameViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
